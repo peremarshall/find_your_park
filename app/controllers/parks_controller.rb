834a805b2fef3606d22f91891107dbb5b897200a
Peregrin Marshall <peremarshall@gmail.com>
2018-12-12 23:43:19 -0700

    finishing up controllers and caching, getting controller tests green

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,11 +1,46 @@
 class ParksController < ApplicationController
 
   def index
+    criteria = build_criteria(params)
+    search = Search.where(searchable_type: "Park", criteria: criteria).first_or_create
+
+    @parks = Rails.cache.fetch("searches/" + search.id.to_s, expires_in: 12.hours) do
+      Park.ransack(params[:q]).result.page(params[:page]).per(params[:per_page]).to_a
+    end
+
+    render :json => { status: 200, data: @parks }
   end
   
   def show
+    @park = Park.find(params[:id])
+    render :json => { status: 200, data: @park }
   end
 
   def refresh
+    nps_client = NationalParkService::Client.new(api_key: Rails.application.credentials.nps[:api_key])
+    nps_parks = nps_client.retrieve_parks
+
+    nps_parks.each do |nps_park|
+      update_park(nps_park)
+    end
+  end
+
+  private
+
+  def build_criteria(params)
+    if params[:q].instance_values["parameters"]
+      criteria = params[:q].instance_values["parameters"].to_hash
+    else
+      criteria = {}
+    end
+
+    criteria["per_page"] = params["per_page"]
+    criteria["page"]     = params["page"]
+    return criteria.sort.to_s
+  end
+
+  def update_park(nps_park)
+    park = Park.find_or_create_by(uuid: nps_park.uuid)
+    park.update_attributes(nps_park.instance_values)
   end
 end
